236733529f1b388b42255b65c7c9988a47b89ccf
augustrs <auso0001@stud.kea.dk>
2024-04-04 10:26:44 +0200

    Initial Commit

---
+++ src/main/java/com/example/wishvault/WishVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishVaultApplication.class, args);
+    }
+
+}
