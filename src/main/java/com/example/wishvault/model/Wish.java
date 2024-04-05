7c4a37edf339a02d67d570096c82f0f13e556090
augustrs <auso0001@stud.kea.dk>
2024-04-05 09:39:54 +0200

    Model class basic structure

--- src/main/java/com/example/wishvault/model/Wish.java
+++ src/main/java/com/example/wishvault/model/Wish.java
@@ -1,4 +1,48 @@
 package com.example.wishvault.model;
 
+
 public class Wish {
+    private String itemName;
+    private String description;
+    private String itemUrl;
+    private double price;
+
+    public Wish(String itemName, String description, String itemUrl, double price) {
+        this.itemName = itemName;
+        this.description = description;
+        this.itemUrl = itemUrl;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getItemUrl() {
+        return itemUrl;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setItemUrl(String itemUrl) {
+        this.itemUrl = itemUrl;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
