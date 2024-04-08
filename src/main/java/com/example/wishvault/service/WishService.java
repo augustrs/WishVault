6356281b83faddf4d4eef626beb86cbe9591ed0d
augustrs <auso0001@stud.kea.dk>
2024-04-08 12:45:30 +0200

    crud

--- src/main/java/com/example/wishvault/service/WishService.java
+++ src/main/java/com/example/wishvault/service/WishService.java
@@ -1,7 +1,17 @@
 package com.example.wishvault.service;
 
+import com.example.wishvault.model.Wishlist;
+import com.example.wishvault.repository.WishRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WishService {
+    private WishRepository wishRepository;
+
+    public WishService(WishRepository wishRepository) {
+        this.wishRepository = wishRepository;
+    }
+
+    //TODO Finish this method
+
 }
