eb8663484b58cc1e30bf7cbfd8e39902c2c2158f
augustrs <auso0001@stud.kea.dk>
2024-04-18 20:55:56 +0200

    demo data sql

--- src/main/resources/templates/demoData.sql
+++ src/main/resources/templates/demoData.sql
@@ -1,12 +1,12 @@
-INSERT INTO WISHLIST (LISTID, WISHERNAME,EVENTNAME) VALUES (1, 'JOAKIM', '12 ÅRS FØDSELSDAG');
-INSERT INTO WISHLIST (LISTID, WISHERNAME,EVENTNAME) VALUES (2, 'MARKUS', '3,5 ÅRS FØDSELSDAG');
-INSERT INTO WISHLIST (LISTID, WISHERNAME,EVENTNAME) VALUES (3, 'AUGUST', 'KONFIRMATION');
+INSERT INTO WISHLIST (LISTID, WISHERNAME,EVENTNAME) VALUES (1, 'JOAKIM', 'Joakims fødselsdag');
+INSERT INTO WISHLIST (LISTID, WISHERNAME,EVENTNAME) VALUES (2, 'MARKUS', 'Markus fødselsdag');
+INSERT INTO WISHLIST (LISTID, WISHERNAME,EVENTNAME) VALUES (3, 'AUGUST', 'Augusts juleønsker');
 
 
-INSERT INTO WISH (WISHID, NAME, DESCRIPTION,ITEMURL,PRICE, LISTID) VALUES (1, 'AirPods', 'De der seje airpods max', 'www.apple.com', 4599, 2);
-INSERT INTO WISH (WISHID, NAME, DESCRIPTION,ITEMURL,PRICE, LISTID) VALUES (2, 'Oplader', 'En MacBook air oplader', 'www.apple.com', 199, 1);
-INSERT INTO WISH (WISHID, NAME, DESCRIPTION,ITEMURL,PRICE, LISTID) VALUES (3, 'Rolex', 'Et datejust', 'www.rolex.com', 45999, 3);
+INSERT INTO WISH (WISHID, NAME, DESCRIPTION,ITEMURL,PRICE, LISTID) VALUES (1, 'AirPods Pro', 'De der seje airpods max', 'https://www.apple.com/dk/airpods-2nd-generation/', 4599, 2);
+INSERT INTO WISH (WISHID, NAME, DESCRIPTION,ITEMURL,PRICE, LISTID) VALUES (2, 'Oplader', 'En MacBook air oplader', 'https://www.apple.com/dk/shop/mac/accessories/charging-essentials?fh=4595%2B45d4', 199, 1);
+INSERT INTO WISH (WISHID, NAME, DESCRIPTION,ITEMURL,PRICE, LISTID) VALUES (3, 'Rolex', 'Et datejust', 'https://www.chrono24.dk/rolex/datejust--mod45.htm', 45999, 3);
 
-INSERT INTO IMAGE(IMAGEID,IMAGE,WISHID) VALUES (1, 'WWW.GOOGLE.COM',1);
-INSERT INTO IMAGE(IMAGEID,IMAGE,WISHID) VALUES (2, 'WWW.GOOGLE.COM',2);
-INSERT INTO IMAGE(IMAGEID,IMAGE,WISHID) VALUES (3, 'WWW.GOOGLE.COM',3);
+INSERT INTO IMAGE(IMAGEID,IMAGE,WISHID) VALUES (1, 'https://www.elgiganten.dk/image/dv_web_D1800010021625045/673048/apple-airpods-pro-2nd-gen-2023-tradlose-horetelefoner-usb-c.jpg',1);
+INSERT INTO IMAGE(IMAGEID,IMAGE,WISHID) VALUES (2, 'https://shop0254.sfstatic.io/upload_dir/shop/_thumbs/Apple-Macbook-magsafe-oplader-96W-Usb-C-kompatibel.w250.h250.fill.webp',2);
+INSERT INTO IMAGE(IMAGEID,IMAGE,WISHID) VALUES (3, 'https://media3.urvaerket.dk/images/product-popup-o/qr/rolex-submariner-126610LN-0001.jpg',3);
